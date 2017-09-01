18bb8fe0ad1b8921972123c89883510bf05c8cc9
Jose A. Jimenez-Berni <berni@ias.csic.es>
2017-09-01 15:15:56 +0200

    Include model for debugging

---
+++ main/pcse/crop/greenlayercrop.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2004-2014 Alterra, Wageningen-UR
+# Allard de Wit (allard.dewit@wur.nl), April 2014
+import datetime
+
+from ..traitlets import Float, Int, Instance, Enum, Unicode
+from ..decorators import prepare_rates, prepare_states
+from ..util import limit
+from ..base_classes import ParamTemplate, StatesTemplate, RatesTemplate, \
+     SimulationObject
+from .. import signals
+from .. import exceptions as exc
+
+
+from .evapotranspiration import Simple_Evapotranspiration as Evapotranspiration
+from .root_dynamics import Simple_Root_Dynamics as Root_Dynamics
+from .leaf_dynamics import CSDM_Leaf_Dynamics as Leaf_Dynamics
+
+class GreenLayerCrop(SimulationObject):
+    """Top level object organizing the different components of the crop
+    simulation for a fake crop which only simulates water use but does not
+    simulate growth of biomass, etc. All crop parameters are hard-coded into the
+    respective modules, only soil parameters can be passed.
+    
+    The FakeCropSimulation is used to put before the real crop starts in order
+    to reduce the effect of water balance initialization.
+            
+    The processes that are implemented as embedded simulation objects consist of:
+    
+        1. Evapotranspiration (self.evtra)
+        2. Leaf dynamics (self.lv_dynamics)
+        3. Root dynamics (self.ro_dynamics)
+
+    **Simulation parameters:**
+    
+    None
+    
+    **State variables:**
+
+    =======  ================================================= ==== ============
+     Name     Description                                      Pbl      Unit
+    =======  ================================================= ==== ============
+    CTRAT    Total crop transpiration                           N    cm
+    DOF      Date representing the day of finish of the crop    N    -
+             simulation. 
+    FINISH   String representing the reason for finishing the   N    -
+             simulation: maturity, harvest, leave death, etc.
+    =======  ================================================= ==== ============
+
+ 
+     **Rate variables:**
+
+    None
+    """
+    
+    # sub-model components for crop simulation
+    evtra = Instance(SimulationObject)
+    lv_dynamics = Instance(SimulationObject)
+    ro_dynamics = Instance(SimulationObject)
+    
+    class StateVariables(StatesTemplate):
+        CTRAT = Float(-99.) # Crop total transpiration
+        DOF = Instance(datetime.date)
+        FINISH = Instance(str)
+        WRSI = Float()
+        SumPET = Float() # Sum of potential crop evapotranspiration
+        SumAET = Float() # Sum of actual crop evapotranspiration
+
+
+    def initialize(self, day, kiosk, parvalues):
+        """
+        :param day: start date of the simulation
+        :param kiosk: variable kiosk of this PCSE instance
+        :param parvalues: `ParameterProvider` object providing parameters as
+                key/value pairs
+        """
+
+        self.kiosk = kiosk
+        
+        # Initialize components of the crop
+        self.evtra = Evapotranspiration(day, kiosk, parvalues)
+        self.ro_dynamics = Root_Dynamics(day, kiosk, parvalues)
+        self.lv_dynamics = Leaf_Dynamics(day, kiosk, parvalues)
+
+        self.states = self.StateVariables(kiosk, CTRAT=0.0, DOF=None, FINISH=None, WRSI=100,
+                                          SumPET=0., SumAET=0.)
+            
+        # assign handler for CROP_FINISH signal
+        self._connect_signal(self._on_CROP_FINISH, signal=signals.crop_finish)
+
+    #---------------------------------------------------------------------------
+    @prepare_rates
+    def calc_rates(self, day, drv):
+        states = self.states
+
+        # (evapo)transpiration rates
+        self.evtra(day, drv)
+
+        # Root growth
+        self.ro_dynamics.calc_rates(day, drv)
+        # leaf growth
+        self.lv_dynamics.calc_rates(day, drv)
+
+    #---------------------------------------------------------------------------
+    @prepare_states
+    def integrate(self, day, delt=1.0):
+        states = self.states
+        
+        # Integrate states on leaves, storage organs, stems and roots
+        self.ro_dynamics.integrate(day, delt)
+        self.lv_dynamics.integrate(day, delt)
+
+        # total crop transpiration (CTRAT)
+        states.CTRAT += self.kiosk["TRA"] * delt
+
+        # Sum of total and actual evapotranspiration
+        states.SumPET += (self.kiosk["TRAMX"] + self.kiosk["EVS"]) * delt
+        states.SumAET += (self.kiosk["TRA"] + self.kiosk["EVS"]) * delt
+        # compute Water Requirements Satisfaction Index according to FAO
+        states.WRSI = states.SumAET/states.SumPET * 100
+        
+    #---------------------------------------------------------------------------
+    def _on_CROP_FINISH(self, day, finish_type, *args, **kwargs):
+        """Handler for setting day of finish (DOF) and reason for
+        crop finishing (FINISH).
+        """
+        self._for_finalize["DOF"] = day
+        self._for_finalize["FINISH"]= finish_type
---
+++ main/pcse/models.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2004-2014 Alterra, Wageningen-UR
+# Allard de Wit (allard.dewit@wur.nl), April 2014
+
+from .engine import Engine
+
+
+class Wofost71_PP(Engine):
+    """Convenience class for running WOFOST7.1 Potential Production.
+
+    :param parameterprovider: A ParameterProvider instance providing all parameter values
+    :param weatherdataprovider: A WeatherDataProvider object
+    :param agromanagement: Agromanagement data
+    """
+    config = "Wofost71_PP.conf"
+
+    def __init__(self, parameterprovider, weatherdataprovider, agromanagement):
+        Engine.__init__(self, parameterprovider, weatherdataprovider, agromanagement,
+                        config=self.config)
+
+
+class Wofost71_WLP_FD(Engine):
+    """Convenience class for running WOFOST7.1 water-limited production.
+
+    :param parameterprovider: A ParameterProvider instance providing all parameter values
+    :param weatherdataprovider: A WeatherDataProvider object
+    :param agromanagement: Agromanagement data
+    """
+    config = "Wofost71_WLP_FD.conf"
+
+    def __init__(self, parameterprovider, weatherdataprovider, agromanagement):
+        Engine.__init__(self, parameterprovider, weatherdataprovider, agromanagement,
+                        config=self.config)
+
+
+class LINTUL3(Engine):
+    """The LINTUL model (Light INTerception and UtiLisation) is a simple general crop model,
+    which simulates dry matter production as the result of light interception and utilization
+    with a constant light use efficiency.
+
+    LINTUL3 simulates crop growth under water-limited and nitrogen-limited conditions
+
+    :param parameterprovider: A `ParameterProvider` object providing model
+        parameters as key/value pairs. The parameterprovider encapsulates
+        the different parameter sets for crop, soil and site parameters.
+    :param weatherdataprovider: An instance of a WeatherDataProvider that can
+        return weather data in a WeatherDataContainer for a given date.
+    :param agromanagement: AgroManagement data. The data format is described
+        in the section on agronomic management.
+    """
+    config = "Lintul3.conf"
+
+    def __init__(self, parameterprovider, weatherdataprovider, agromanagement):
+        Engine.__init__(self, parameterprovider, weatherdataprovider, agromanagement,
+                        config=self.config)
+
+
+class FAO_WRSI_Potential(Engine):
+    """Convenience function for computing potential crop water requirements with a (modified) FAO WRSI approach.
+
+    :param parameterprovider: A ParameterProvider instance providing all parameter values
+    :param weatherdataprovider: A WeatherDataProvider object
+    :param agromanagement: Agromanagement data
+    """
+
+    config = "GreenLayerCrop_PP.conf"
+
+    def __init__(self, parameterprovider, weatherdataprovider, agromanagement):
+        Engine.__init__(self, parameterprovider, weatherdataprovider, agromanagement,
+                        config=self.config)
+
+
+class FAO_WRSI_WaterLimited(Engine):
+    """Convenience function for computing actual crop water use including the Water Requirements
+    Satisfaction Index with a (modified) FAO WRSI approach.
+
+    :param parameterprovider: A ParameterProvider instance providing all parameter values
+    :param weatherdataprovider: A WeatherDataProvider object
+    :param agromanagement: Agromanagement data
+    """
+
+    config = "GreenLayerCrop_WLP.conf"
+
+    def __init__(self, parameterprovider, weatherdataprovider, agromanagement):
+        Engine.__init__(self, parameterprovider, weatherdataprovider, agromanagement,
+                        config=self.config)
